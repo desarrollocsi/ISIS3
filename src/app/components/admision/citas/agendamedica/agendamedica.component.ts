57963b9e2b169dd7de42325e471691456118cfed
Yur Adan Valdez Carazas <yvaldez@clinicasantaisabel.com>
2020-12-28 16:13:14 -0500

    se agrego formulario acto medico,pacientes citados

--- src/app/components/admision/citas/agendamedica/agendamedica.component.ts
+++ src/app/components/admision/citas/agendamedica/agendamedica.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-agendamedica',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgendamedicaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSave(){
+    this.router.navigate(['admision/citas/pacientescitados'])
   }
 
 }
--- src/app/components/admision/citas/pacientescitados/pacientescitados.component.ts
+++ src/app/components/admision/citas/pacientescitados/pacientescitados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pacientescitados',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PacientescitadosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+
   }
+
+
+  acto(){
+    this.router.navigate(['admision/citas/agendamedica'])
+  }
+
 
 }
